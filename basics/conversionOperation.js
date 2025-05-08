df7e29994581f72ce7d23caee3bfc303067ffdb8
Jarvis-258 <157241798+Jarvis-258@users.noreply.github.com>
2025-05-08 07:01:01 +0000

    data

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,21 +1,45 @@
-//let score = 33
-//let score = "33abc"
-//let score = null
-//let score = undefined
-// let score = true
+// //let score = 33
+// //let score = "33abc"
+// //let score = null
+// //let score = undefined
+// // let score = true
 
-// console.log(typeof score);
-// console.log(typeof(score));
+// // console.log(typeof score);
+// // console.log(typeof(score));
 
-let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
 
-// "33" = 33
-// "33abc" = NaN
-// true = 1; false = 0
+// // "33" = 33
+// // "33abc" = NaN
+// // true = 1; false = 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// ************************* Oprations ***************************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "Hello"
+let str2 = "Bro"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
---
+++ basics/datatype-summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// 7 type : String, Number, Boolearn, null, undefined, Symbol, BigIn
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = true
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId );
+//const bigNumber = 45676545677654336543n
+
+// Reference (Non-Primitive)
+
+// Array, Function, Object
+
+const heros = ["Ironman", "Hanuman", "Superman"]
+let Myobj = {
+    name :"OM",
+    age : 18,
+}
+
+// Used function variable
+
+const MyFunction = function()
+{
+console.log("Hello Word");
+
+}
+
+console.log(typeof bigNumber);
