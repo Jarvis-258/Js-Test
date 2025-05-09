2be1f9917731ef272dc647a42a36b6fc4650c262
Jarvis-258 <157241798+Jarvis-258@users.noreply.github.com>
2025-05-09 15:33:04 +0000

    control_flow

---
+++ control_flow/one.js
@@ -0,0 +1,84 @@
+// // if
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if (temperature === 41 )
+// {
+//    //console.log("less then 50");
+   
+// }
+//   else 
+//   {
+//  //console.log("temperature is greater then 50");
+//   }
+//  //console.log("Execute");
+ 
+
+
+// {/* < lessthen
+// > greaterthen
+// <= greatherthen or equelto
+// >= lessthen or equelto
+// == tocheck
+// != notequal
+// ===  
+// !==
+// */}
+
+
+// // const score = 200
+// // if (score > 100) 
+// // {
+// //     const power = "fly"
+// //     console.log(`User power: ${power}`);
+    
+// // }
+
+// // short hand notesion //
+
+// const balance = 1000
+
+// if (balance > 500)
+// {
+//     // console.log("test");
+//     // console.log("test2");
+    
+    
+// }
+
+// if (balance < 500)
+// {
+// console.log("less then 500");
+
+// } 
+// else if (balance < 750)
+// {
+//    console.log("less then 750"); 
+// }
+//  else if (balance < 900)
+// {
+//    console.log("less then 900"); 
+// }
+// else 
+
+  
+// {
+//    console.log("less then 1200"); 
+// }
+
+
+const userloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userloggedIn && debitCard && 2==3)
+{
+    console.log("Allow to buy course");
+    
+}
+if (loggedInFromGoogle || loggedInFromEmail )
+{
+    console.log("User logged in");
+    
+}
